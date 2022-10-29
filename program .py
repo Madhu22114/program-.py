cc9dbe62b423acb446df10e2f6f8b9bc16e9641d
Abhi22114 <114794064+Abhi22114@users.noreply.github.com>
2022-10-29 10:59:51 +0530

    Add files via upload

---
+++ program .py
@@ -0,0 +1,22 @@
+"hello world"
+print('hello world' )
+x=70
+y=80
+print('x+y')
+print(x+y)
+x=[10,20.30]
+print(type(x))
+y=4.5
+print(type(y))
+a=40
+print(type(a))
+z="my name is abhijeet"
+print(z)
+print(type(z))
+t=8837863947
+print(t)
+print(type(t))
+w="Programing"
+print(w[0])
+print(w[5])
+print(w[9])
